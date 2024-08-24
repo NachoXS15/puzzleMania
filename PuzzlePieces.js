f08654ea4f336e7c9763c08be69b2372963f95b0
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-08-24 04:51:03 -0300

    fixing route

--- PuzzlePieces.js
+++ PuzzlePieces.js
@@ -1,192 +1,192 @@
 const pieces = [
   {
     block: 1, // Cambié el nombre de la propiedad
-    referenceImg: "pieces/block1/reference1.jpg",
+    referenceImg: "./pieces/block1/reference1.jpg",
     pieces: [
       {
         pieceID: 1,
-        pieceImg: "pieces/block1/Pieza 1.jpg",
+        pieceImg: "./pieces/block1/Pieza 1.jpg",
       },
       {
         pieceID: 2,
-        pieceImg: "pieces/block1/Pieza 2.jpg",
+        pieceImg: "./pieces/block1/Pieza 2.jpg",
       },
       {
         pieceID: 3,
-        pieceImg: "pieces/block1/Pieza 3.jpg",
+        pieceImg: "./pieces/block1/Pieza 3.jpg",
       },
       {
         pieceID: 4,
-        pieceImg: "pieces/block1/Pieza 4.jpg",
+        pieceImg: "./pieces/block1/Pieza 4.jpg",
       },
       {
         pieceID: 5,
-        pieceImg: "pieces/block1/Pieza 5.jpg",
+        pieceImg: "./pieces/block1/Pieza 5.jpg",
       },
       {
         pieceID: 6,
-        pieceImg: "pieces/block1/Pieza 6.jpg",
+        pieceImg: "./pieces/block1/Pieza 6.jpg",
       },
       {
         pieceID: 7,
-        pieceImg: "pieces/block1/Pieza 7.jpg",
+        pieceImg: "./pieces/block1/Pieza 7.jpg",
       },
       {
         pieceID: 8,
-        pieceImg: "pieces/block1/Pieza 8.jpg",
+        pieceImg: "./pieces/block1/Pieza 8.jpg",
       },
 
     ],
   },
   {
     block: 2,
-    referenceImg: "pieces/block2/reference2.jpg",
+    referenceImg: "./pieces/block2/reference2.jpg",
     pieces: [
       {
         pieceID: 1,
-        pieceImg: "pieces/block2/Pieza 1.jpg",
+        pieceImg: "./pieces/block2/Pieza 1.jpg",
       },
       {
         pieceID: 2,
-        pieceImg: "pieces/block2/Pieza 2.jpg",
+        pieceImg: "./pieces/block2/Pieza 2.jpg",
       },
       {
         pieceID: 3,
-        pieceImg: "pieces/block2/Pieza 3.jpg",
+        pieceImg: "./pieces/block2/Pieza 3.jpg",
       },
       {
         pieceID: 4,
-        pieceImg: "pieces/block2/Pieza 4.jpg",
+        pieceImg: "./pieces/block2/Pieza 4.jpg",
       },
       {
         pieceID: 5,
-        pieceImg: "pieces/block2/Pieza 5.jpg",
+        pieceImg: "./pieces/block2/Pieza 5.jpg",
       },
       {
         pieceID: 6,
-        pieceImg: "pieces/block2/Pieza 6.jpg",
+        pieceImg: "./pieces/block2/Pieza 6.jpg",
       },
       {
         pieceID: 7,
-        pieceImg: "pieces/block2/Pieza 7.jpg",
+        pieceImg: "./pieces/block2/Pieza 7.jpg",
       },
       {
         pieceID: 8,
-        pieceImg: "pieces/block2/Pieza 8.jpg",
+        pieceImg: "./pieces/block2/Pieza 8.jpg",
       },
     ],
   },
   {
     block: 3,
-    referenceImg: "pieces/block3/reference3.jpg",
+    referenceImg: "./pieces/block3/reference3.jpg",
     pieces: [
       {
         pieceID: 1,
-        pieceImg: "pieces/block3/Pieza 1.jpg",
+        pieceImg: "./pieces/block3/Pieza 1.jpg",
       },
       {
         pieceID: 2,
-        pieceImg: "pieces/block3/Pieza 2.jpg",
+        pieceImg: "./pieces/block3/Pieza 2.jpg",
       },
       {
         pieceID: 3,
-        pieceImg: "pieces/block3/Pieza 3.jpg",
+        pieceImg: "./pieces/block3/Pieza 3.jpg",
       },
       {
         pieceID: 4,
-        pieceImg: "pieces/block3/Pieza 4.jpg",
+        pieceImg: "./pieces/block3/Pieza 4.jpg",
       },
       {
         pieceID: 5,
-        pieceImg: "pieces/block3/Pieza 5.jpg",
+        pieceImg: "./pieces/block3/Pieza 5.jpg",
       },
       {
         pieceID: 6,
-        pieceImg: "pieces/block3/Pieza 6.jpg",
+        pieceImg: "./pieces/block3/Pieza 6.jpg",
       },
       {
         pieceID: 7,
-        pieceImg: "pieces/block3/Pieza 7.jpg",
+        pieceImg: "./pieces/block3/Pieza 7.jpg",
       },
       {
         pieceID: 8,
-        pieceImg: "pieces/block3/Pieza 8.jpg",
+        pieceImg: "./pieces/block3/Pieza 8.jpg",
       },
     ],
   },
   {
     block: 4,
-    referenceImg: "pieces/block4/reference4.jpg",
+    referenceImg: "./pieces/block4/reference4.jpg",
     pieces: [
       {
         pieceID: 1,
-        pieceImg: "pieces/block4/Pieza 1.jpg",
+        pieceImg: "./pieces/block4/Pieza 1.jpg",
       },
       {
         pieceID: 2,
-        pieceImg: "pieces/block4/Pieza 2.jpg",
+        pieceImg: "./pieces/block4/Pieza 2.jpg",
       },
       {
         pieceID: 3,
-        pieceImg: "pieces/block4/Pieza 3.jpg",
+        pieceImg: "./pieces/block4/Pieza 3.jpg",
       },
       {
         pieceID: 4,
-        pieceImg: "pieces/block4/Pieza 4.jpg",
+        pieceImg: "./pieces/block4/Pieza 4.jpg",
       },
       {
         pieceID: 5,
-        pieceImg: "pieces/block4/Pieza 5.jpg",
+        pieceImg: "./pieces/block4/Pieza 5.jpg",
       },
       {
         pieceID: 6,
-        pieceImg: "pieces/block4/Pieza 6.jpg",
+        pieceImg: "./pieces/block4/Pieza 6.jpg",
       },
       {
         pieceID: 7,
-        pieceImg: "pieces/block4/Pieza 7.jpg",
+        pieceImg: "./pieces/block4/Pieza 7.jpg",
       },
       {
         pieceID: 8,
-        pieceImg: "pieces/block4/Pieza 8.jpg",
+        pieceImg: "./pieces/block4/Pieza 8.jpg",
       },
     ],
   },
   {
     block: 5,
-    referenceImg: "pieces/block5/reference5.jpg",
+    referenceImg: "./pieces/block5/reference5.jpg",
     pieces: [
       {
         pieceID: 1,
-        pieceImg: "pieces/block5/Pieza 1.jpg",
+        pieceImg: "./pieces/block5/Pieza 1.jpg",
       },
       {
         pieceID: 2,
-        pieceImg: "pieces/block5/Pieza 2.jpg",
+        pieceImg: "./pieces/block5/Pieza 2.jpg",
       },
       {
         pieceID: 3,
-        pieceImg: "pieces/block5/Pieza 3.jpg",
+        pieceImg: "./pieces/block5/Pieza 3.jpg",
       },
       {
         pieceID: 4,
-        pieceImg: "pieces/block5/Pieza 4.jpg",
+        pieceImg: "./pieces/block5/Pieza 4.jpg",
       },
       {
         pieceID: 5,
-        pieceImg: "pieces/block5/Pieza 5.jpg",
+        pieceImg: "./pieces/block5/Pieza 5.jpg",
       },
       {
         pieceID: 6,
-        pieceImg: "pieces/block5/Pieza 6.jpg",
+        pieceImg: "./pieces/block5/Pieza 6.jpg",
       },
       {
         pieceID: 7,
-        pieceImg: "pieces/block5/Pieza 7.jpg",
+        pieceImg: "./pieces/block5/Pieza 7.jpg",
       },
       {
         pieceID: 8,
-        pieceImg: "pieces/block5/Pieza 8.jpg",
+        pieceImg: "./pieces/block5/Pieza 8.jpg",
       }
     ],
   }   
